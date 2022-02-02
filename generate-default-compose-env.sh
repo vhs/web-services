4699cdd2b1aa7b60fe65a503999e163a6a18781f
Ty Eggen <tycho.eggen@gmail.com>
2022-02-02 13:16:02 -0800

    Various: file cleanup and SoC for base/prod service definitions

--- generate-default-compose-env.sh
+++ generate-default-compose-env.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-( echo docker-compose.yml ; find services/* -name service.yml | egrep -v _template ) | tr '\n' ':' | perl -pe 's/:$//g' | awk '{ print "COMPOSE_FILE=" $0 }' > env.default
+(
+    echo docker-compose.yml
+    find services/* -name 'service.*.yml' | egrep -v _template
+) | tr '\n' ':' | perl -pe 's/:$//g' | awk '{ print "COMPOSE_FILE=" $0 }' >env.default
